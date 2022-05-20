d5ce874964d7754d04e1ce55c35fee43faf555ca
najmul-rusu <nhrusu@gmail.com>
2022-05-21 01:47:53 +0600

    Database connection

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-
+//? middle were
 app.use(cors());
 app.use(express.json());
 
+//? Database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-01.x4mie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
